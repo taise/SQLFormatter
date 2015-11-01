b6fe682b8996ed72b9f6c95288532b587e236234
taise <taise515@gmail.com>
2015-11-01 15:45:54 +0900

    Add a simple lexical analyser

--- app/lexer.js
+++ app/lexer.js
@@ -1,16 +1,52 @@
 var Lexer = function(sql) {
   this.sql = sql;
 
-  var tokenizeKeywords = [
+  this.reservedWord = [
     'SELECT',
-    'FROM',
-    'WHERE',
-    'GROUP BY',
-    'ORDER BY',
-    'LIMIT'
-  ];
-  var LEFT_PAREN ='(';
-  var RIGHT_PAREN =')';
+    'FROM'
+  ]
+  this.TERMINAL_SYMBOL = ';';
+  this.LEFT_PAREN ='(';
+  this.RIGHT_PAREN =')';
+
+  Lexer.prototype.term_list = function() {
+    return this.sql.replace(/;$/, " ;").split(" ");
+  };
+
+  Lexer.prototype.tokenize = function() {
+    /*
+     * Return
+     * ------
+     * [[reservedWord, token, indentLevel](, ...)]
+     */
+    var result = [];
+
+    var indentLevel = 0;
+    var that = this;
+    that.term_list().forEach(function(token) {
+        var i = that.reservedWord.indexOf(token);
+        if (i >= 0) {
+          result.push([
+              token,
+              that.reservedWord[i],
+              indentLevel
+          ]);
+        } else if(token === that.TERMINAL_SYMBOL) {
+          result.push([
+              token,
+              'TERMINAL_SYMBOL',
+              indentLevel
+          ]);
+        } else {
+          result.push([
+              token,
+              'LITERAL',
+              indentLevel + 1
+          ]);
+        }
+    });
+    return result;
+  };
 };
 
 module.exports = Lexer;
--- test/test_lexer.js
+++ test/test_lexer.js
@@ -1,18 +1,39 @@
 var assert = require('assert');
 var Lexer = require('../app/lexer.js');
 
-
 describe('Lexer', function() {
     var sql = 'SELECT * FROM users;'
+    var lexer = new Lexer(sql);
     describe('new', function() {
-        var lexer = new Lexer(sql);
-
         it('should return self instance', function () {
             assert(lexer instanceof Lexer);
         });
 
         it('should return the sql', function(){
-            assert.equal(sql, lexer.sql);
+            assert.equal(lexer.sql, sql);
+        });
+    });
+
+    describe('#term_list', function() {
+        it('should splited array by a space', function () {
+            var expect = ['SELECT', '*', 'FROM', 'users', ';'];
+            assert.deepEqual(lexer.term_list(), expect);
+        });
+    });
+
+    describe('#tokenize', function() {
+        it('should return a tokenized array', function () {
+            var expect = [ ['SELECT', 'SELECT', 0],
+              ['*', 'LITERAL', 1],
+              ['FROM', 'FROM', 0],
+              ['users', 'LITERAL', 1],
+              [';', 'TERMINAL_SYMBOL', 0] ];
+            var actual = lexer.tokenize();
+
+            assert.equal(actual.length, expect.length);
+            for(var i = 0; i < actual.length; i++) {
+              assert.deepEqual(actual[i], expect[i]);
+            }
         });
     });
 });
